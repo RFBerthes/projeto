1b0dd50cdb169ff56a7d2f9149027d089f4193bf
RFBerthes <rfbds102030@gmail.com>
2019-05-07 02:31:21 -0300

    login.php

---
+++ src/login.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br">
+<body>
+    <?php
+        echo "<p>Dados recebidos</p>";
+        echo "<p>Nome:".$_POST["usuario"]."</p>";
+        echo "<p>Nome:".$_POST["senha"]."</p>";
+
+        
+
+    ?>
+</body>
+</html>
