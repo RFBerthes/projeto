9784d79cd290b49827185c17d45385ed55f2ebae
RFBerthes <rfbds102030@gmail.com>
2019-05-16 16:53:13 -0300

    update

--- src/atendente.php
+++ src/atendente.php
@@ -58,8 +58,19 @@
     </script>
   </head>
   <body class="bg-dark text-white">
-
-
+      <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-3 bg-secondary" >
+              <p>Lorem ipsum...</p>
+            </div>
+            <div class="col-sm-7 bg-white text-dark" >
+              <p>Sed ut perspiciatis...</p>
+            </div>
+            <div class="col-sm-2" style="background-color:pink;">
+                <p>Sed ut perspiciatis...</p>
+              </div>
+          </div>
+        </div> 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
