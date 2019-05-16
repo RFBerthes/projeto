7990f587e85bf3e917dc632f1bc48243196c6a03
RFBerthes <rfbds102030@gmail.com>
2019-05-16 00:30:30 -0300

    inicio atendente

---
+++ src/atendente.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Admin</title>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+      <a class="navbar-brand" href="admin.php">Peça&Pag</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="admin.php">HOME <span class="sr-only">(página atual)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ADMINISTRAÇÃO
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="sabores.php">Sabores   </a>
+              <a class="dropdown-item" href="pizzas.php">Pizzas     </a>
+              <a class="dropdown-item" href="bebidas.php">Bebidas   </a>
+              <a class="dropdown-item" href="usuarios.php">Usuários </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="mesas.php">MESAS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">RELATÓRIOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">BACKUP</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="index.php">SAIR</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+          <button class="btn btn-outline-success my-2 my-sm-0 bg-dark" type="submit">OK</button>
+        </form>
+      </div>
+    </nav>
+
+    <script>
+
+    </script>
+  </head>
+  <body class="bg-dark text-white">
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
